320b97a9fadc93a5462d9816853acab5d59924b8
Pavel-Zhabotinskii <111363588+Pavel-Zhabotinskii@users.noreply.github.com>
2023-04-14 13:34:00 +0200

    changed link in Navigation

--- src/components/Navigation/Navigatoin.tsx
+++ src/components/Navigation/Navigatoin.tsx
@@ -1,32 +1,40 @@
-import React, {FC, useState} from "react";
-import cl from "./Navigation.module.scss"
+import React, { FC, useState } from 'react';
+import cl from './Navigation.module.scss';
+import Link from 'next/link';
 // import { useTranslation } from "react-i18next";
 
-interface IItemsList{
-    href: string,
-    value: any
+interface IItemsList {
+	href: string;
+	value: any;
 }
 
 interface INavication {
-    items?: IItemsList[],
-    flex?: boolean,
+	items?: IItemsList[];
+	flex?: boolean;
 }
 
-const Navigation: FC<INavication> = ({items, flex = false}) => {
-    // const {t} = useTranslation()
+const Navigation: FC<INavication> = ({ items, flex = false }) => {
+	// const {t} = useTranslation()
 
+	return (
+		<nav
+			className={
+				flex
+					? [cl.navigation, cl.flex].join(' ')
+					: [cl.navigation, cl.block].join(' ')
+			}
+		>
+			<ul className={''}>
+				{items!.map(item => (
+					<li key={item.value}>
+						<Link href={item.href} className={''}>
+							{item.value}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+};
 
-    return(
-        <nav className={flex ? [cl.navigation, cl.flex].join(' ') : [cl.navigation, cl.block].join(' ') }>
-            <ul className={''}>
-                {items!.map(item => 
-                    <li key={item.value}>
-                        <a href={item.href} className={""}>{(item.value)}</a>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    )
-}
-
-export default Navigation
+export default Navigation;
