fd3e92bc3cb8b51b32b0d76b30ae1f14b34f6021
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-04-16 12:05:16 +0300

    merge

--- next.config.js
+++ next.config.js
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-// const nextConfig = {
-//     reactStrictMode: true,
-//     i18n: {
-//         locales: ['en', 'ru'],
-//         defaultLocale: 'ru',
-//         localeDetection: true,
-//     }
-// }
-//
-// module.exports = nextConfig
-=======
-const nextConfig = {
-  reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'ru'],
-    defaultLocale: 'ru',
-    localeDetection: true,
-  }
-}
->>>>>>> main
-
 const nextTranslate = require('next-translate-plugin')
 
 module.exports = nextTranslate({
